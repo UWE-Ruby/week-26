258d1a48645ffddd2ebdeb6ade9af801f9da5dbe
Franklin Webber <franklin.webber@gmail.com>
2012-05-22 18:45:04 -0700

    FIX route show_user was not present

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_in_path_for(resource)
-    show_user_path(resource)
+    user_path(resource)
   end
 
   def after_inactive_sign_up_path_for(resource)
